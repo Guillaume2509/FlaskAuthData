0f5373e1ca32599642376048c4e101b3318860b6
Guillaume Giroux <45839232+Guillaume2509@users.noreply.github.com>
2021-02-18 13:52:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 TODO: Create VENV
   Venv pip install flask
   Venv pip install flask-login
-  venv pip install flask-sqlalchemy
+  Venv pip install flask-sqlalchemy
   
+from website import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
